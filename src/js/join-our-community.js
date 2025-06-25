10537a2ac148c3ecb4f6f08e04ca2fe804b4ccc9
Eks-Vadzim Simanau <eks220885@gmail.com>
2025-06-25 13:03:24 +0300

    create custom swiper

--- src/js/join-our-community.js
+++ src/js/join-our-community.js
@@ -1,30 +1,8 @@
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
+import { initializeSwiper } from './swiper-custom.js';
 
-const swiper = new Swiper('.mySwiper', {
-  direction: 'horizontal',
-  slidesPerView: 1,
-  spaceBetween: 0,
-  loop: false,
-
-  navigation: {
-    nextEl: '.join-button.swiper-button-next',
-    prevEl: '.join-button.swiper-button-prev',
-  },
-
-  breakpoints: {
-    0: {
-      enabled: true,
-      slidesPerView: 1,
-      spaceBetween: 35,
-      loop: true,
-    },
-
-    1200: {
-      enabled: false,
-      slidesPerView: 3,
-      spaceBetween: 153,
-      loop: false,
-    },
-  },
-});
+const joinCommunitySwiper = initializeSwiper(
+  '.mySwiper',
+  {},
+  '.join-button.swiper-button-next',
+  '.join-button.swiper-button-prev'
+);
