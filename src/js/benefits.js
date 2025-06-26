d22602924133b1d0eca1e6174ced360a27c6dc27
Eks-Vadzim Simanau <eks220885@gmail.com>
2025-06-26 20:55:47 +0300

    fixing problems

--- src/js/benefits.js
+++ src/js/benefits.js
@@ -6,27 +6,19 @@ document.addEventListener('DOMContentLoaded', () => {
     {
       grabCursor: true,
 
-      // Дадаем опцыі для больш дакладнага абнаўлення Swiper
-      // Гэтыя опцыі дапамагаюць Swiper рэагаваць на змены DOM і пераразлічваць памеры
-      observer: true, // Назірае за зменамі ўнутры самога Swiper
-      observeParents: true, // Назірае за зменамі ў бацькоўскіх элементах Swiper
-      observeSlideChildren: true, // Назірае за зменамі ў даччыных элементах слайдаў
+      observer: true,
+      observeParents: true,
+      observeSlideChildren: true,
 
-      // Калі пасля анімацыі слайд "скача", можна паспрабаваць скінуць transition-duration
-      // або паэксперыментаваць з хуткасцю.
-      // Непасрэднае заданне transition-duration: 0 у CSS можа зламаць анімацыю.
-      // Лепш кіраваць гэтым праз JS, калі ёсць такая неабходнасць.
-
-      // Для перастрахоўкі, выклікаем update() пасля ініцыялізацыі
       on: {
         init: function () {
           this.update();
         },
-        // А таксама пры кожным "slideChangeTransitionEnd" - калі анімацыя спынілася
+
         slideChangeTransitionEnd: function () {
-          this.update(); // Прымушаем Swiper пераразлічыць пазіцыі пасля завяршэння пераходу
+          this.update();
         },
-        // Ці нават пры "transitionEnd", калі слайды візуальна не заканчваюць рух дакладна
+
         transitionEnd: function () {
           this.update();
         },
@@ -39,7 +31,6 @@ document.addEventListener('DOMContentLoaded', () => {
           centeredSlides: true,
           spaceBetween: 10,
           loop: true,
-          // centeredSlidesOffset: -26, // Выдаляем, бо пачатковая пазіцыя ідэальная
         },
         1200: {
           enabled: true,
